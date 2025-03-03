250576b08ba5ea45f866a983188631fd64051e9b
Bodruzzaman Ansari NAFI <101050010+nafi001@users.noreply.github.com>
2025-03-03 16:02:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_chat import message
 import tempfile
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
